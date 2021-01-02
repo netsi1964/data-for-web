00d42eb35fc04014502e1056172020e255abfc76
Sten Hougaard <netsi1964@gmail.com>
2021-01-02 14:09:44 +0100

    Fetched latest CSV files

---
+++ code/index.js
@@ -0,0 +1,38 @@
+const brain = require("brain.js");
+const data = require("../Deaths_over_time.json");
+
+let deaths = 0;
+const trainData = data.map((day) => {
+  let key = Object.keys(day)[0];
+  let temp = key.split("-");
+  let date = new Date(temp[0], temp[1] - 1, temp[2]);
+  let deathsToday = parseInt(day[key]);
+  let rise = deathsToday > deaths ? 1 : 0;
+  deaths = deathsToday;
+  return {
+    input: getInput(date),
+    output: [rise],
+  };
+});
+
+const net = new brain.NeuralNetwork();
+net.train(trainData);
+
+console.log(trainData);
+
+let day = 24 * 60 * 60 * 1000;
+let today = new Date();
+let lastDeaths = 24;
+for (var i = 0; i < 30; i++) {
+  const prediction = net.run(getInput(today));
+  let mul = (prediction[0] - 0.5) * 2 + 1;
+  lastDeaths = mul * lastDeaths;
+  console.log(today.toLocaleDateString(), parseInt(lastDeaths + 0.5));
+
+  today = new Date(today.getTime() + day);
+}
+
+function getInput(date) {
+  const input = [date.getDay()];
+  return input;
+}
